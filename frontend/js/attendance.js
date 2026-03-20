312efefe95c8a539988ff45bda948597c0951096
Manali Patel <pmanali196@gmail.com>
2026-03-20 14:39:25 +0530

    Changes in models

--- frontend/js/attendance.js
+++ frontend/js/attendance.js
@@ -8,7 +8,7 @@ async function loadModels(){
     await faceapi.nets.tinyFaceDetector.loadFromUri('frontend/models');
     await faceapi.nets.faceLandmark68Net.loadFromUri('frontend/models');
     await faceapi.nets.faceRecognitionNet.loadFromUri('frontend/models');
-    await faceapi.nets.tiny_yolov2.loadFromUri('frontend/models');
+    // await faceapi.nets.tiny_yolov2.loadFromUri('frontend/models');
 
     statusText.innerText = "Models Loaded";
 }
