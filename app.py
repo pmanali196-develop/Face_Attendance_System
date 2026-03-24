f70f1a18e16549fc118511123a9e5cf9c3ed0bb4
Manali Patel <pmanali196@gmail.com>
2026-03-24 20:31:14 +0530

    Fixed CORS policy

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if not firebase_admin._apps:
         cred = credentials.Certificate("firebase_key.json")
 
     firebase_admin.initialize_app(cred, {
-        'storageBucket': 'online-attendance-system-5b66b.firebasestorage.app'
+        'storageBucket': 'online-attendance-system-5b66b.appspot.com'
     })
 
 db = firestore.client()
